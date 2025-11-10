02e9f3c45b3a8786a807cb9138fe27e8e613e2cc
mariacweir-blip <mariacweir@gmail.com>
2025-11-10 00:01:47 +0000

    Typo causing error fixed

--- app_clean.py
+++ app_clean.py
@@ -14,7 +14,8 @@ except Exception:
     from ic_licai.exporters import save_to_local  # fallback
 
 st.set_page_config(page_title="IC-LicAI Expert Console", layout="centered")
-ss = st.session_stat
+ss = st.session_state
+
 
 
 # ---------- UI CONSTANTS ----------
